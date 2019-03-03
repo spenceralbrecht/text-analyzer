2700dc0d0b052cae3ab114d612e4f2f2af4da061
Spencer Albrecht <spencera@usc.edu>
2019-03-02 19:22:26 -0800

    Displaying contacts with more than 10 messages

--- flutter_texter/lib/main.dart
+++ flutter_texter/lib/main.dart
@@ -3,22 +3,13 @@ import 'package:sms/sms.dart';
 import 'package:sms/contact.dart';
 
 void main() {
-  getMessages();
   runApp(new MyApp());
 }
 
-getMessages() async {
+Future<List<String>>_getMessages() async {
   SmsQuery query = new SmsQuery();
-//  List<SmsMessage> messages = await query.getAllSms;
-//  List<SmsMessage> messages = await query.querySms({
-//    address: getContactAddress()
-//  });
-//  ContactQuery contacts = new ContactQuery();
-//  Contact contact = await contacts.queryContact('8176370103');
+  ContactQuery contacts = new ContactQuery();
   List<SmsMessage> messages = await query.querySms();
-//  query.querySms({0, 2, contact.address});
-//  query.querySms()
-  var count = 0;
   var contactMap = new Map();
   for (var i=0; i < messages.length; i++) {
     SmsMessage message = messages[i];
@@ -30,16 +21,33 @@ getMessages() async {
       contactMap[phoneNumber] = new List<SmsMessage>();
     }
   }
-  void iterateMapEntry(key, value) async {
-    // Check if I have more than 10 messages with this person
-    if (value.length > 10) {
-      ContactQuery contacts = new ContactQuery();
-      Contact contact = await contacts.queryContact(key);
-      print(contact.fullName);
-    }
+//  void iterateMapEntry(key, value) async {
+//    // Check if I have more than 10 messages with this person
+//    if (value.length < 10) {
+//      contactMap.remove(key);
+//    }
+//  }
+
+//  contactMap.forEach(iterateMapEntry);
+  contactMap.removeWhere((key, value) => value.length < 10);
+  
+  var contactNumbers = contactMap.keys.toList();
+  List<String> contactNames = new List<String>();
+  for (var i=0; i<contactNumbers.length; i++) {
+    Contact contact = await contacts.queryContact(contactNumbers[i]);
+    contactNames.add(contact.fullName);
   }
-  contactMap.forEach(iterateMapEntry);
+
+//  List<String> contactNames = contactNumbers.map((number) => contacts.queryContact(number).toString()).toList();
+  print(contactNames);
+  return contactNames;
+//  return contactMap;
 //  print(contactMap.keys);
+}
+
+getTilesForContacts(contactMap) {
+  List<Widget> tiles = new List<ListTile>();
+
 }
 
 class MyApp extends StatelessWidget {
@@ -52,7 +60,23 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text("Yeahhhhh"),
+          child: new FutureBuilder(
+            future: _getMessages(), // a previously-obtained Future<String> or null
+               builder: (BuildContext context, AsyncSnapshot snapshot) {
+                 switch (snapshot.connectionState) {
+                   case ConnectionState.none:
+                     return Text('Press button to start.');
+                   case ConnectionState.active:
+                   case ConnectionState.waiting:
+                     return Text('Awaiting result...');
+                   case ConnectionState.done:
+                     if (snapshot.hasError)
+                       return Text('Error: ${snapshot.error}');
+                     return Text('Result: ${snapshot.data}');
+                 }
+                 return null; // unreachable
+               },
+          )
         ),
       ),
     );
