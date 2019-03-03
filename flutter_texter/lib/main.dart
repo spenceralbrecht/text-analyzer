7ea5f937c41fd562b3ceed934660c223baee5b52
Spencer Albrecht <spencera@usc.edu>
2019-03-02 22:54:17 -0800

    Add spacing and remove phone number from card list

--- flutter_texter/lib/main.dart
+++ flutter_texter/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:sms/sms.dart';
 import 'package:sms/contact.dart';
 
 void main() {
-  runApp(new MyApp());
+    runApp(new MyApp());
 }
 
 sendText(number, message) {
@@ -14,21 +14,21 @@ sendText(number, message) {
 
 Future<Map<Contact, List<SmsMessage>>>_getMessages() async {
 
-  SmsQuery query = new SmsQuery();
-  var contactMap = new Map<Contact, List<SmsMessage>>();
-  List<SmsThread> threads = await query.getAllThreads;
-  for (var i=0; i<threads.length; i++) {
-    String phoneNumber = threads[i].address;
-    ContactQuery contactQuery = new ContactQuery();
-    Contact contact = await contactQuery.queryContact(phoneNumber);
-    contactMap[contact] = (threads[i].messages);
+    SmsQuery query = new SmsQuery();
+    var contactMap = new Map<Contact, List<SmsMessage>>();
+    List<SmsThread> threads = await query.getAllThreads;
+    for (var i=0; i<threads.length; i++) {
+        String phoneNumber = threads[i].address;
+        ContactQuery contactQuery = new ContactQuery();
+        Contact contact = await contactQuery.queryContact(phoneNumber);
+        contactMap[contact] = (threads[i].messages);
 
-    // Print messages for a specific number
+        // Print messages for a specific number
 //    if (contact.address == '9499107853') {
 //      List<SmsMessage> messages = contactMap[contact];
 //      messages.forEach((m) => print(m.body));
 //    }
-  }
+    }
 
 
 //  for (var i=0; i < messages.length; i++) {
@@ -53,9 +53,9 @@ Future<Map<Contact, List<SmsMessage>>>_getMessages() async {
 //  }
 
 //  contactMap.forEach(iterateMapEntry);
-  contactMap.removeWhere((key, value) => value.length < 10);
-  return contactMap;
-  
+    contactMap.removeWhere((key, value) => value.length < 10);
+    return contactMap;
+
 //  var contactNumbers = contactMap.keys.toList();
 //  List<String> contactNames = new List<String>();
 //  for (var i=0; i<contactNumbers.length; i++) {
@@ -71,94 +71,104 @@ Future<Map<Contact, List<SmsMessage>>>_getMessages() async {
 }
 
 getTilesForContacts(contactMap) {
-  List<Widget> tiles = new List<ListTile>();
+    List<Widget> tiles = new List<ListTile>();
 
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Text Analyzer',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: Text('Contacts'),
-        ),
-        body: Center(
-          child: new FutureBuilder(
-            future: _getMessages(), // a previously-obtained Future<String> or null
-               builder: (BuildContext context, AsyncSnapshot snapshot) {
-                 switch (snapshot.connectionState) {
-                   case ConnectionState.none:
-                   case ConnectionState.active:
-                   case ConnectionState.waiting:
-                     return Text('Loading...');
-                   case ConnectionState.done:
-                     if (snapshot.hasError)
-                       return Text('Error: ${snapshot.error}');
-                     return createListView(context, snapshot);
-                 }
-                 return null; // unreachable
-               },
-          )
-        ),
-      ),
-    );
-  }
-
-  Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
-//    List<String> values = snapshot.data;
-    Map<Contact, List<SmsMessage>> contactMap = snapshot.data;
-    List<Contact> contacts = contactMap.keys.toList();
-    List<String> names = contacts.map((contact) => contact.fullName).toList();
-    List<String> numbers = contacts.map((contact) => contact.address).toList();
-    List<int> numChatMessages = contacts.map((contact) => contactMap[contact].length).toList();
-
-    List<Person> people = new List<Person>();
-    for (var i=0; i<names.length; i++) {
-      people.add(new Person(names[i], numbers[i], numChatMessages[i]));
+    @override
+    Widget build(BuildContext context) {
+        return new MaterialApp(
+            title: 'Text Analyzer',
+            home: new Scaffold(
+                appBar: new AppBar(
+                    title: Text('Contacts'),
+                ),
+                body: Center(
+                  child: new FutureBuilder(
+                      future: _getMessages(), // a previously-obtained Future<String> or null
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                          switch (snapshot.connectionState) {
+                              case ConnectionState.none:
+                              case ConnectionState.active:
+                              case ConnectionState.waiting:
+                                  return Text('Loading...');
+                              case ConnectionState.done:
+                                  if (snapshot.hasError)
+                                      return Text('Error: ${snapshot.error}');
+                                  return createListView(context, snapshot);
+                          }
+                          return null; // unreachable
+                      },
+                  )
+                ),
+            ),
+        );
     }
 
+    Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+//    List<String> values = snapshot.data;
+        Map<Contact, List<SmsMessage>> contactMap = snapshot.data;
+        List<Contact> contacts = contactMap.keys.toList();
+        List<String> names = contacts.map((contact) => contact.fullName).toList();
+        List<String> numbers = contacts.map((contact) => contact.address).toList();
+        List<int> numChatMessages = contacts.map((contact) => contactMap[contact].length).toList();
+
+        List<Person> people = new List<Person>();
+        for (var i=0; i<names.length; i++) {
+            people.add(new Person(names[i], numbers[i], numChatMessages[i]));
+        }
+
 //    .sort((a, b) => a.id.compareTo(b.id));
-    people.sort((a, b) => b.numMessages.compareTo(a.numMessages));
-    people.forEach((person) => print(person.numMessages));
+        people.sort((a, b) => b.numMessages.compareTo(a.numMessages));
+        people.forEach((person) => print(person.numMessages));
 //    values.forEach((contact) => (contact.fullName));
 //    map((name) => name.fullName);
-    return new ListView.builder(
+        return new ListView.builder(
 //      padding: EdgeInsets.all(8.0),
-      itemCount: people.length,
+            itemCount: people.length,
 //      itemExtent: 20.0,
-      itemBuilder: (BuildContext context, int index) {
-        return new Column(
-          children: <Widget>[
-            new ListTile(
-              title: new Text(
-                  people[index].name,
-                  style: TextStyle(fontWeight: FontWeight.w600),
-                  textScaleFactor: 1.5,
-              ),
-              subtitle: new Text(people[index].number),
-              trailing: new Text(
-                people[index].numMessages.toString(),
-                  style: TextStyle(fontWeight: FontWeight.w600),
-                  textScaleFactor: 1.4,
-              ),
-            ),
-            new Divider(
-              height: 2.0,
-            )
-          ],
+            itemBuilder: (BuildContext context, int index) {
+                return new Column(
+                    children: <Widget>[
+                        new ListTile(
+                            title: new Text(
+                                people[index].name,
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                                textScaleFactor: 1.5,
+                            ),
+                            trailing: new Column(
+                                children: <Widget>[
+                                    Text(
+                                        people[index].numMessages.toString(),
+                                        style: TextStyle(fontWeight: FontWeight.w600),
+                                        textScaleFactor: 1.4,
+                                    ),
+                                    Text(
+                                        'messages',
+                                        style: TextStyle(fontWeight: FontWeight.w300),
+                                    ),
+                                ],
+                            ),
+                            contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                            onTap: () { /* react to the tile being tapped */ },
+
+                        ),
+                        new Divider(
+                            height: 2.0,
+                        )
+                    ],
+                );
+            },
         );
-      },
-    );
-  }
+    }
 }
 
 class Person {
-  String name;
-  String number;
-  int numMessages;
+    String name;
+    String number;
+    int numMessages;
 
-  Person(this.name, this.number, this.numMessages) {}
+    Person(this.name, this.number, this.numMessages) {}
 
 }
