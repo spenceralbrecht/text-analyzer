8c85804e63ac2affac5dd1e6e1983fcccd8797fe
Spencer Albrecht <spencera@usc.edu>
2019-03-02 21:32:02 -0800

    Swap smsQuery to include sent messages as well

--- flutter_texter/lib/main.dart
+++ flutter_texter/lib/main.dart
@@ -6,21 +6,45 @@ void main() {
   runApp(new MyApp());
 }
 
-Future<List<String>>_getMessages() async {
+sendText(number, message) {
+    SmsSender sender = new SmsSender();
+    String address = number;
+    sender.sendSms(new SmsMessage(address, message));
+}
+
+Future<Map<Contact, List<SmsMessage>>>_getMessages() async {
+
   SmsQuery query = new SmsQuery();
-  ContactQuery contacts = new ContactQuery();
-  List<SmsMessage> messages = await query.querySms();
-  var contactMap = new Map();
-  for (var i=0; i < messages.length; i++) {
-    SmsMessage message = messages[i];
-    String phoneNumber = message.address;
-    if (contactMap.containsKey(phoneNumber)) {
-      contactMap[phoneNumber].add(message);
-    }
-    else {
-      contactMap[phoneNumber] = new List<SmsMessage>();
-    }
+  var contactMap = new Map<Contact, List<SmsMessage>>();
+  List<SmsThread> threads = await query.getAllThreads;
+  for (var i=0; i<threads.length; i++) {
+    String phoneNumber = threads[i].address;
+    ContactQuery contactQuery = new ContactQuery();
+    Contact contact = await contactQuery.queryContact(phoneNumber);
+    contactMap[contact] = (threads[i].messages);
+
+    // Print messages for a specific number
+//    if (contact.address == '9499107853') {
+//      List<SmsMessage> messages = contactMap[contact];
+//      messages.forEach((m) => print(m.body));
+//    }
   }
+
+
+//  for (var i=0; i < messages.length; i++) {
+//    SmsMessage message = messages[i];
+//    String phoneNumber = message.address;
+//    if (contactMap.containsKey(phoneNumber)) {
+////      if (phoneNumber == '8176370103') {
+////      if (phoneNumber == '9092476022') {
+////        print(message.body);
+////      }
+//      contactMap[phoneNumber].add(message);
+//    }
+//    else {
+//      contactMap[phoneNumber] = new List<SmsMessage>();
+//    }
+//  }
 //  void iterateMapEntry(key, value) async {
 //    // Check if I have more than 10 messages with this person
 //    if (value.length < 10) {
@@ -30,17 +54,18 @@ Future<List<String>>_getMessages() async {
 
 //  contactMap.forEach(iterateMapEntry);
   contactMap.removeWhere((key, value) => value.length < 10);
+  return contactMap;
   
-  var contactNumbers = contactMap.keys.toList();
-  List<String> contactNames = new List<String>();
-  for (var i=0; i<contactNumbers.length; i++) {
-    Contact contact = await contacts.queryContact(contactNumbers[i]);
-    contactNames.add(contact.fullName);
-  }
+//  var contactNumbers = contactMap.keys.toList();
+//  List<String> contactNames = new List<String>();
+//  for (var i=0; i<contactNumbers.length; i++) {
+//    Contact contact = await contacts.queryContact(contactNumbers[i]);
+//    contactNames.add(contact.fullName);
+//  }
 
 //  List<String> contactNames = contactNumbers.map((number) => contacts.queryContact(number).toString()).toList();
-  print(contactNames);
-  return contactNames;
+//  print(contactNames);
+//  return contactNames;
 //  return contactMap;
 //  print(contactMap.keys);
 }
@@ -54,10 +79,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Text Analyzer',
       home: new Scaffold(
         appBar: new AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Contacts'),
         ),
         body: Center(
           child: new FutureBuilder(
@@ -82,16 +107,28 @@ class MyApp extends StatelessWidget {
   }
 
   Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
-    List<String> values = snapshot.data;
+//    List<String> values = snapshot.data;
+    Map<Contact, List<SmsMessage>> contactMap = snapshot.data;
+    List<Contact> contacts = contactMap.keys.toList();
+    List<String> names = contacts.map((contact) => contact.fullName).toList();
+    List<String> numbers = contacts.map((contact) => contact.address).toList();
+    List<int> numChatMessages = contacts.map((contact) => contactMap[contact].length).toList();
+//    values.forEach((contact) => (contact.fullName));
+//    map((name) => name.fullName);
     return new ListView.builder(
 //      padding: EdgeInsets.all(8.0),
-      itemCount: values.length,
+      itemCount: names.length,
 //      itemExtent: 20.0,
       itemBuilder: (BuildContext context, int index) {
         return new Column(
           children: <Widget>[
             new ListTile(
-              title: new Text(values[index]),
+              title: new Text(
+                  names[index],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textScaleFactor: 1.5,
+              ),
+              subtitle: new Text(numbers[index]),
             ),
             new Divider(
               height: 2.0,
